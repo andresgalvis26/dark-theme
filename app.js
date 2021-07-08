ecff510328ce89e57bd322cfd3fba8b2474d9274
ROG-AndresGalvis <andres.felipe99@hotmail.com>
2021-07-08 11:36:40 -0500

    Sitio web básico con light y dark theme

---
+++ app.js
@@ -0,0 +1,28 @@
+const button = document.getElementById("btnCambiar");
+const configUser = window.matchMedia("(prefers-color-scheme: dark)");
+const localConfig = localStorage.getItem("tema");
+
+if(localConfig === "dark"){
+    document.body.classList.toggle("dark-theme");
+} else if (localConfig === "light"){
+    document.body.classList.toggle("light-theme");
+}
+
+button.addEventListener("click", () => {
+    //console.log(configUser.matches);
+
+    let colorTema;
+
+    if(configUser.matches){
+        document.body.classList.toggle("light-theme");
+
+        colorTema = document.body.classList.contains("light-theme") ? "light" : "dark"
+
+    } else {
+        document.body.classList.toggle("dark-theme");
+
+        colorTema = document.body.classList.contains("dark-theme") ? "dark" : "light"
+    }
+
+    localStorage.setItem("tema", colorTema);
+})
